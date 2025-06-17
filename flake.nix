1702a3aa54f49b87dd9a09ef2509e676ba4eb62c
toyboot4e <toyboot4e@gmail.com>
2025-06-17 20:53:13 +0900

    add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+	    emacs
+	    zenn-cli
+          ];
+        };
+      });
+}
